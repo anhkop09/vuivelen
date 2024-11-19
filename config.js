255e02aaa1e6d709aa0b90a11b0ea31e0464ecd1
anhkop09 <106652809+anhkop09@users.noreply.github.com>
2024-11-19 13:03:20 +0700

    vuile

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Anh có điều muốn nói....!
+    `,
+    btnIntro: '❤️❤️❤️',
+    title: 'Mình quay lại với nhau nhé❤️ ',
+    desc: 'Anh vẫn còn nhớ iem nhìu lắm ',
+    btnYes: 'Có, vẫn nhớ anh nhiều lắm',
+    btnNo: 'Không, anh đang nằm mơ à',
+    question: 'Thật lun hả trời',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vâng ạ',
+    mess: 'Bọn tao chỉ thử lòng mày thôi. Thằng đấy đang ôm con khác rồi,mày nghĩ thằng đấy muốn quay lại à con BÒ này. TRÁI TIM mày chưa VỠ đủ à ⚠️ ',
+    messDesc: 'Sự thật chỉ có một mà thoiiii, tỉnh táo giùm',
+    btnAccept: 'Tối nằm mà ăn năn hối cải đê...',
+    messLink: 'https://www.facebook.com/chenggg30122003' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
